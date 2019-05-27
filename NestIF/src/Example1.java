4d94d0765b93c6a2e8b5234fe558c1aac66ba298
modernduck <sompop.kulapalanont@gmail.com>
2019-05-27 10:46:55 +0700

    first case of under 60

--- NestIF/src/Example1.java
+++ NestIF/src/Example1.java
@@ -11,7 +11,23 @@ public class Example1 {
 		System.out.print("Please input your salary per year?:");
 		int salary = number.nextInt();
 		
-		
+		int tax = -1;
+		if(age < 60) {
+			if(salary < 250000) {
+				tax = 0;
+			}else if( salary < 500000) {
+				tax = 10;
+			}else if( salary < 1000000) {
+				tax = 20;
+			}else {
+				tax = 30;
+			}
+		}else if(age < 80) {
+			
+		}else {
+			
+		}
+		System.out.println("Your tax is " + tax + " %");
 	}
 
 }
