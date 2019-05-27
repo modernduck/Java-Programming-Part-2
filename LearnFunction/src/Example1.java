193f5253d40f761c616d9634f778fb19b36e88d3
modernduck <sompop.kulapalanont@gmail.com>
2019-05-27 13:21:55 +0700

    start project

--- LearnFunction/src/Example1.java
+++ LearnFunction/src/Example1.java
@@ -16,15 +16,19 @@ public class Example1 {
 		System.out.println("THIS IS MY FIRST TEST FUNCTION");
 	}
 	
-	public static int findMax() {
-		return 555;
+	public static int findMax(int a, int b) {
+		if( a > b) {
+			return a;
+		}else {
+			return b;
+		}
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("============testing na kub =======");
 		//test();
-		int max = findMax();
+		int max = findMax(999, 5);
 		System.out.println("max is " + max);
 	}
 
---
+++ LearnFunction/src/FindTax.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class FindTax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner number = new Scanner(System.in);
+		System.out.print("How old are you?:");
+		int age = number.nextInt();
+		System.out.print("Please input your salary per year?:");
+		int salary = number.nextInt();
+	}
+
+}
