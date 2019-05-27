548b6751e39fdc081b2889f5c05926c4af66a709
modernduck <sompop.kulapalanont@gmail.com>
2019-05-27 13:24:45 +0700

    skelaton

--- LearnFunction/src/FindTax.java
+++ LearnFunction/src/FindTax.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 public class FindTax {
 
+	
+	public static int getTax(int age, int salary) {
+		
+		
+		
+		return -1;
+	}
+	
+	public static int calculateTax(int tax, int salary) {
+		
+		return -2;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner number = new Scanner(System.in);
@@ -8,6 +21,11 @@ public class FindTax {
 		int age = number.nextInt();
 		System.out.print("Please input your salary per year?:");
 		int salary = number.nextInt();
+		//kumnuan
+		int tax = getTax(age, salary);
+		int cost = calculateTax(tax, salary);
+		//output
+		System.out.println("Your tax is " + tax + "% so you have to pay " + cost + " THB"); 
 	}
 
 }
