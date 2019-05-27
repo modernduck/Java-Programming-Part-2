e3f07e346da148c8a11f02814cc551ad452a3b91
modernduck <sompop.kulapalanont@gmail.com>
2019-05-27 12:58:40 +0700

    update

--- SwitchCase/src/SwitchCase.java
+++ SwitchCase/src/SwitchCase.java
@@ -1,9 +1,23 @@
-
+import java.util.Scanner;
 public class SwitchCase {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner number = new Scanner(System.in);
+		int a = number.nextInt();
+		switch( a ){
+			case 1:
+				System.out.println("THIS IS FROM CASE ONE");
+				break;
+			case 3:
+				System.out.println("THIS IS FROM CASE 3");
+				break;
+			case 5:
+				System.out.println("THIS IS FROM CASE 5");
+				break;
+			default:
+				System.out.println("DEFAULT");
+		}
+		
 	}
 
 }
