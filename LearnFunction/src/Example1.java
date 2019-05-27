6f3700ce43607df3a5cee6745e93b1ca95720d75
modernduck <sompop.kulapalanont@gmail.com>
2019-05-27 13:11:27 +0700

    test void

---
+++ LearnFunction/src/Example1.java
@@ -0,0 +1,25 @@
+
+public class Example1 {
+
+	public static void test() {
+		System.out.println("THIS IS MY FIRST TEST FUNCTION");
+		System.out.println("THIS IS MY FIRST TEST FUNCTION");
+		System.out.println("THIS IS MY FIRST TEST FUNCTION");
+		System.out.println("THIS IS MY FIRST TEST FUNCTION");
+		System.out.println("THIS IS MY FIRST TEST FUNCTION");
+		System.out.println("THIS IS MY FIRST TEST FUNCTION");
+		System.out.println("THIS IS MY FIRST TEST FUNCTION");
+		System.out.println("THIS IS MY FIRST TEST FUNCTION");
+		System.out.println("THIS IS MY FIRST TEST FUNCTION");
+		System.out.println("THIS IS MY FIRST TEST FUNCTION");
+		System.out.println("THIS IS MY FIRST TEST FUNCTION");
+		System.out.println("THIS IS MY FIRST TEST FUNCTION");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("============testing na kub =======");
+		test();
+	}
+
+}
