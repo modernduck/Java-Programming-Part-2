a3151a656b99229172eda483a2f6717996ac678d
modernduck <sompop.kulapalanont@gmail.com>
2019-05-27 13:13:46 +0700

    initial return int
    find max return int

--- LearnFunction/src/Example1.java
+++ LearnFunction/src/Example1.java
@@ -16,10 +16,16 @@ public class Example1 {
 		System.out.println("THIS IS MY FIRST TEST FUNCTION");
 	}
 	
+	public static int findMax() {
+		return 555;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("============testing na kub =======");
-		test();
+		//test();
+		int max = findMax();
+		System.out.println("max is " + max);
 	}
 
 }
